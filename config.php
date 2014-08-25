d6b4cd7e5dd3652cef54c72530746647ab407693
Jean Paul <jeanpaulrodrigues@hotmail.com>
2014-08-25 03:41:18 -0300

    CRUD PDO + Bootstrap

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$conn = 'mysql:host=localhost;dbname=pdo';
+
+try {
+	$db = new PDO($conn, 'root', '');
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	if($e->getCode() == 1049){
+		echo "Erro no banco de dados JEANWEB.COM.BR.";
+	}else{
+		echo $e->getMessage();
+	}
+}
+?>
